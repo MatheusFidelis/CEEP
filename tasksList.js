f5c3e6ad1992c8419d2d44b76d2b5351aab39852
matheusfidelis <matheusfidelis2@gmail.com>
2021-10-21 14:23:47 -0300

    Create the task as a function

--- tasksList.js
+++ tasksList.js
@@ -1,5 +1,12 @@
+const createTask = (event) => {
+  event.preventDefault();
+
+  const input = document.querySelector("[data-form-input]");
+  const inputValue = input.value;
+  console.log(inputValue);
+  input.value = " ";
+};
+
 const newTask = document.querySelector("[data-form-button]");
 
-newTask.addEventListener("click", () => {
-  console.log("I've been clicked");
-});
+newTask.addEventListener("click", createTask);
